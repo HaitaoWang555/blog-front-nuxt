4d14d436e32808b144521f34c9e15ac62f508ba9
wanghaitao <15124505701@163.com>
2020-05-24 13:43:11 +0800

    project init

---
+++ components/ScrollTop.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-fab-transition>
+    <v-btn
+      v-show="!hidden"
+      :elevation="15"
+      fixed
+      bottom
+      right
+      fab
+      color="red"
+      @click="$vuetify.goTo(target, options)"
+    >
+      <v-icon color="white">mdi-arrow-up</v-icon>
+    </v-btn>
+  </v-fab-transition>
+</template>
+
+<script>
+export default {
+  name: 'ScrollTop',
+  data() {
+    return {
+      hidden: true,
+      duration: 1000,
+      offset: 0,
+      easing: 'easeInOutQuart'
+    }
+  },
+  computed: {
+    target() {
+      return 'html'
+    },
+    options() {
+      return {
+        duration: this.duration,
+        offset: this.offset,
+        easing: this.easing
+      }
+    }
+  },
+  mounted() {
+    // 顶部图标的显示与隐藏
+    window.onscroll = () => {
+      if (document.documentElement.scrollTop > 200) {
+        this.hidden = false
+      } else {
+        this.hidden = true
+      }
+    }
+  }
+}
+</script>
---
+++ nuxt.config.js
@@ -0,0 +1,78 @@
+const isDev = process.env.NODE_ENV === 'development'
+
+export default {
+  mode: 'universal',
+  /*
+   ** Headers of the page
+   */
+  head: {
+    titleTemplate: '%s - ' + process.env.npm_package_name,
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      {
+        hid: 'description',
+        name: 'description',
+        content: process.env.npm_package_description || ''
+      }
+    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+  },
+  /*
+   ** Customize the progress-bar color
+   */
+  loading: { color: '#fff' },
+  /*
+   ** Global CSS
+   */
+  css: [
+    { src: '~/assets/style.scss' },
+    '@mdi/font/css/materialdesignicons.css'
+  ],
+  /*
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
+  /*
+   ** Nuxt.js dev-modules
+   */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/vuetify'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [],
+  /*
+   ** vuetify module configuration
+   ** https://github.com/nuxt-community/vuetify-module
+   */
+  vuetify: {
+    customVariables: ['~/assets/variables.scss'],
+    defaultAssets: false,
+    treeShake: true,
+    theme: {
+      dark: true
+    }
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    filenames: {
+      app: isDev ? '[name].js' : 'js/[name]-[chunkhash:7].js',
+      chunk: isDev ? '[name].js' : 'js/[name]-[chunkhash:7].js',
+      vendor: isDev ? '[name].js' : 'js/[name]-[chunkhash:7].js',
+      css: isDev ? '[name].css' : 'css/[name]-[chunkhash:7].css'
+    },
+    extractCSS: !isDev,
+    html: { minify: { collapseWhitespace: true } },
+    /*
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
+  }
+}
